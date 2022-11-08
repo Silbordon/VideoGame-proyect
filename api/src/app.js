920f31d7cbd4504bbcacbe559a182da5334c616a
silvina <silbordon.89@gmail.com>
2022-11-08 01:24:14 -0300

    add env vars

--- api/src/app.js
+++ api/src/app.js
@@ -1,8 +1,10 @@
 const express = require('express');
+require("dotenv").config();
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const routes = require('./routes/index.js');
+const { DOMAIN } = process.env;
 
 require('./db.js');
 
@@ -15,7 +17,7 @@ server.use(bodyParser.json({ limit: '50mb' }));
 server.use(cookieParser());
 server.use(morgan('dev'));
 server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Origin', `http://${DOMAIN}:3000`); // update to match the domain you will make the request from
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
--- client/src/redux/actions/index.js
+++ client/src/redux/actions/index.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+require("dotenv").config();
 export const GET_ALL_VIDEOGAMES = "GET_ALL_VIDEOGAMES";
 export const GET_VIDEOGAMES_BYNAME = "GET_VIDEOGAMES_BYNAME";
 export const GET_GENRES = "GET_GENRES";
@@ -10,14 +11,14 @@ export const FILTER_SOURCE = "FILTER_SOURCE";
 export const ORDER_BY_ALFABETIC = "ORDER_BY_ALFABETIC";
 export const ORDER_BY_RATING = "ORDER_BY_RATING";
 export const CHANGE_LOADER = "CHANGE_LOADER";
-// export const REFRESH_VIDEOGAME_DETAILS = "REFRESH_VIDEOGAME_DETAILS"
-// export const CREATE_VIDEOGAME = "CREATE_VIDEOGAME";
-// export const DELETE_VIDEOGAME = "DELETE_VIDEOGAME";
+const { DOMAIN } = process.env;
+
+
 
 export const getAllVideogames = () => {
   return async function (dispatch) {
     try {
-      let respuesta = await axios.get("http://localhost:3001/videogames");
+      let respuesta = await axios.get(`http://${DOMAIN}:3001/videogames`);
       return dispatch({
         type: GET_ALL_VIDEOGAMES,
         payload: respuesta.data,
